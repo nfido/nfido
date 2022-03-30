a0fcfd68c40d705baa3d9cf40175dcc1211120a7
cnmade <hufeng1987@gmail.com>
2022-03-30 13:01:04 +0800

    继续完成email发送流程

--- src/modules/account/controllers/misc_controller.rs
+++ src/modules/account/controllers/misc_controller.rs
@@ -1,14 +1,27 @@
-
-use actix_web::{get, post, error, Error, HttpResponse, web};
+use actix_session::Session;
+use actix_web::{get, post, error, Error, HttpResponse, web, Responder, Result};
 use crate::AppConfig;
+use crate::common::vo::common_result::CommonResult;
 
 #[get("/account/sendVerifyEmail")]
-pub async fn send_verify_email(tmpl: web::Data<tera::Tera>, conf: web::Data<AppConfig>) -> Result<HttpResponse, Error>{
-    let s = tmpl.render("account/login.html", &tera::Context::new())
-        .map_err(|_| error::ErrorInternalServerError("Termplate error"));
+pub async fn send_verify_email(session: Session,
+                               conf: web::Data<AppConfig>) -> Result<impl Responder> {
 
-    log::info!("The site name: {}", conf.site_name.to_owned());
-    Ok(HttpResponse::Ok().content_type("text/html").body(s.unwrap()))
+    if session.get::<Option<i64>>("uid").is_err() {
+      return  Ok(web::Json(CommonResult::<String> {
+            code: 0,
+            msg: "请先登录".to_string(),
+            data: None,
+        }))
+    }
+
+    //TODO 查用户资料，发邮件
+    return Ok(web::Json(CommonResult::<String> {
+        code: 0,
+        msg: "己经发送".to_string(),
+        data: None,
+    }))
+
 }
 
 #[post("/account/emailVerify")]
