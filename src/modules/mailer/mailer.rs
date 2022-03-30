fa4d55faa77a82ce30cb0293e34843287896bceb
cnmade <hufeng1987@gmail.com>
2022-03-30 14:59:28 +0800

    初步实现发送邮件的逻辑

--- src/modules/mailer/mailer.rs
+++ src/modules/mailer/mailer.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 pub trait MailDelivery {
-    fn send_email(&self, email_address: String, title: String, content: String) -> Result<(), Err> ;
+    fn send_email(&self, email_address: String, title: String, content: String) -> Result<&str, Box<dyn Error>> ;
 }
--- src/modules/mailer/mailer_smtp.rs
+++ src/modules/mailer/mailer_smtp.rs
@@ -1,14 +1,46 @@
+use std::error::Error;
 use crate::mailer::mailer::MailDelivery;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
 
 pub struct MailerSmtp {
     pub smtp_host: String,
     pub smtp_port: String,
     pub smtp_user: String,
     pub smtp_password: String,
+    pub from_email: String,
 }
 
 impl MailDelivery for MailerSmtp {
-    fn send_email(&self, email_address: String, title: String, content: String) -> Result<(), Err> {
+    fn send_email(&self,
+                  email_address: String,
+                  title: String,
+                  content: String) -> Result<&str, Box<dyn Error>> {
         //发送邮件，用smtp方式
+        let email = Message::builder()
+            .from(format!("NFido <{}>", &self.from_email).parse().unwrap())
+            .to(email_address.parse().unwrap())
+            .subject(title)
+            .body(content)
+            .unwrap();
+
+        let creds = Credentials::new(self.smtp_user.to_string(),
+                                     self.smtp_password.to_string());
+
+        let mailer = SmtpTransport::relay(&format!("{}:{}", &self.smtp_host , &self.smtp_port))
+            .unwrap()
+            .credentials(creds)
+            .build();
+
+        match mailer.send(&email) {
+            Ok(_) => {
+                log::info!("Email sent successfully!");
+                return Ok("success");
+            }
+            Err(e) => {
+                log::info!("Could not send email: {:?}", e);
+                return Err(Box::try_from(e).unwrap());
+            }
+        }
     }
 }
